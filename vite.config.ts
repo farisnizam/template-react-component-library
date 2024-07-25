98a6afe20e84e836376045f66174e5980e3dcb99
farisnizam <farismtq@gmail.com>
2024-07-25 19:05:14 +0800

    add import aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,32 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dts from 'vite-plugin-dts'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import path from "path";
 
 export default defineConfig({
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-			'@less': path.resolve(__dirname, './src/less'),
-			'@modules': path.resolve(__dirname, './src/modules'),
-		},
-	},
-	plugins: [react(), dts({ include: ['src'] })],
-	css: {
-		preprocessorOptions: {
-			less: {
-				javascriptEnabled: true,
-				math: 'always',
-			},
-		},
-	},
-	test: {
-		globals: true,
-		environment: 'jsdom',
-	},
-	optimizeDeps: {
-		exclude: ['@storybook/addon-docs'],
-	},
-})
+  resolve: {
+    alias: {
+      "@less": path.resolve(__dirname, "./src/less"),
+      "@modules": path.resolve(__dirname, "./src/modules"),
+    },
+  },
+  plugins: [react(), dts({ include: ["src"] })],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        math: "always",
+      },
+    },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+  optimizeDeps: {
+    exclude: ["@storybook/addon-docs"],
+  },
+});
